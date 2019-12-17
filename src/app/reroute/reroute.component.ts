1605f8aadc5c334d3b301882fb6b42168051657f
mFelgate <m.felgate@outlook.com>
2019-12-17 13:04:28 -0500

    add security checks for redirect

--- src/app/reroute/reroute.component.ts
+++ src/app/reroute/reroute.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { ShortUrlService } from '../../services/short-url.service';
 import { TinyURL } from '../../models/url';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-reroute',
@@ -15,17 +16,24 @@ export class RerouteComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private shortUrlService: ShortUrlService
+    private shortUrlService: ShortUrlService,
+    private toastr: ToastrService
     ) { }
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
-    this.shortUrlService.getFullUrl(id)
-.subscribe(
-      res => {
-        window.location.href = 'https://' + decodeURIComponent(res.fullUrl);
-      }
-    );
+    const safeRedirect = new RegExp('^[a-z0-9]*$');
+    if (id.match(safeRedirect)) {
+      this.shortUrlService.getFullUrl(id).subscribe(
+            res => {
+              window.location.href = 'https://' + decodeURIComponent(res.fullUrl);
+            },
+            error => {
+              this.toastr.error('Opps!', 'Could not find URL');
+            }
+          );
+        } else {
+          this.toastr.error('Opps!', 'The short URL does not look right');
+        }
+    }
   }
-
-}
